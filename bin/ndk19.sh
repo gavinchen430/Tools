1ca6bad3316169cb5a541c60067e364063aabda5
gavinchen <15601910741@163.com>
2021-02-21 11:37:16 +0800

    vim config and common command

---
+++ bin/ndk19.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -n "$ANDROID_NDK" ];then
+ANDROID_NDK=${ANDROID_NDK}:
+export PATH=`echo $PATH | sed "s#${ANDROID_NDK}##g"`
+fi
+export ANDROID_NDK=/home/gavinchen/tools/ndk/android-ndk-r19c
+export ANDROID_ROOT_DIR=$ANDROID_NDK
+export ANDROID_NDK_HOME=$ANDROID_NDK
+export PATH=$ANDROID_NDK:$PATH
+echo ANDROID_NDK_HOME: $ANDROID_NDK
+echo PATH: $PATH
